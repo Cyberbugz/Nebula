1da0c97078296bf305a3deeab06d56571e3aa1b1
Manson <mohamed.ibrahim@westore.ai>
2023-03-17 10:44:30 +0200

    Check if guard option exists

--- app/Console/Core/Concerns/OptionsExtender.php
+++ app/Console/Core/Concerns/OptionsExtender.php
@@ -10,16 +10,30 @@ trait OptionsExtender
 
     protected function getOptions(): array
     {
-        return array_merge(parent::getOptions(), [
+        $options = array_merge(parent::getOptions(), [
             ['module', 'M', InputOption::VALUE_REQUIRED, 'Specify a module.'],
             ['absolute', 'A', InputOption::VALUE_OPTIONAL, 'Specify absolute modules path.'],
-            ['guard', 'G', InputOption::VALUE_OPTIONAL, 'Specify guard environment.'],
         ]);
+
+        if (!$this->hasGuardOption($options)) {
+            $options[] = ['guard', 'G', InputOption::VALUE_OPTIONAL, 'Specify guard environment.'];
+        }
+        return $options;
     }
 
     public function handle(): void
     {
         $this->checkAbsolutePath();
         parent::handle();
+    }
+
+    protected function hasGuardOption(array $options): bool
+    {
+        foreach ($options as $option) {
+            if ($option[0] === 'guard') {
+                return true;
+            }
+        }
+        return false;
     }
 }
