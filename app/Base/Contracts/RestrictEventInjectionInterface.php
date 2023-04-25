a6655e2c444b5a51211e683c1baeeccd91c8ea36
Manson <mohamed.ibrahim@westore.ai>
2023-04-25 21:41:50 +0200

    Update structure

--- app/Base/Contracts/RestrictEventInjectionInterface.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace App\Base\Contracts;
-
-interface RestrictEventInjectionInterface
-{
-}
--- app/Support/Enum/Contracts/OptionableInterface.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace App\Support\Enum\Contracts;
-
-interface OptionableInterface
-{
-    public static function options(): array;
-}
