575251d1562c844c83f72c3d5d77923b9e8d8183
Manson <mohamed.ibrahim@westore.ai>
2023-06-25 14:51:09 +0300

    Disable subtitute bindings middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -20,7 +20,7 @@ class Kernel extends HttpKernel
         'api' => [
             //             \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             \Illuminate\Routing\Middleware\ThrottleRequests::class.':api',
-            \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            //            \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
         'test' => [
             \App\Http\Middleware\ForbidProductionRequests::class,
